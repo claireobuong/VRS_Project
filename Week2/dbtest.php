86c57fbd0d6ae5f71439afe999cdac20e6eac14a
claireobuong <claireobuong@gmail.com>
2026-05-28 23:36:42 +0300

    Completed Week 2 wireframes and GUI design

---
+++ Week2/dbtest.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "week2db");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Database connected successfully.";
+
+?>
